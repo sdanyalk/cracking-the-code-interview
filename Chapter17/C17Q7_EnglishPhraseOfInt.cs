2a8fd203a7423ab8f7cadcec1e928433282f7e93
SDK <s.danyal.k@gmail.com>
2015-10-07 16:07:50 -0500

    Q7 - Convert Integer to English Phrase
    Unsolved

---
+++ Chapter17/C17Q7_EnglishPhraseOfInt.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Given any integer, print an English phrase that describes the integer (e.g., "OneThousand, Two Hundred Thirty Four").
+ */
+
+namespace CrackingTheCodeInterview.Chapter17
+{
+    class C17Q7_EnglishPhraseOfInt
+    {
+        static void Main(string[] args)
+        { }
+    }
+}
